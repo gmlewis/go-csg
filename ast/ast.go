881ac09d4ab93428979ec5105e16d3caa6c2a5de
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-12-23 22:52:03 -0500

    Add ast

---
+++ ast/ast.go
@@ -0,0 +1,57 @@
+// Package ast defines the Abstract Syntax Tree for our language.
+package ast
+
+import "github.com/gmlewis/go-monkey/token"
+
+// Node represents a node in our abstract syntax tree.
+type Node interface {
+	TokenLiteral() string
+}
+
+// Statement represents a statement in our abstract syntax tree.
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Expression respresents an expression in our abstract syntax tree.
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program represents a program using our abstract syntax tree.
+type Program struct {
+	Statements []Statement
+}
+
+// TokenLiteral returns the first token literal of the program.
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
+// LetStatement represents a 'let' statement.
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+// TokenLiteral returns the token literal.
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+// Identifier represents an identifier.
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+// TokenLiteral returns the token literal.
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
