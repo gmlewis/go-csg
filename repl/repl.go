9c9aa62310533c991ea475069c5f39cd5f3aab24
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-12-27 09:56:52 -0500

    Add more eval support

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/gmlewis/go-monkey/evaluator"
 	"github.com/gmlewis/go-monkey/lexer"
 	"github.com/gmlewis/go-monkey/parser"
 )
@@ -34,8 +35,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
