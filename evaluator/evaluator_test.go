ead7770294d80f2bbcea8d1313c905d4a20a98b0
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-12-27 11:51:02 -0500

    Add closure test

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -233,6 +233,13 @@ func TestFunctionApplication(t *testing.T) {
 	}
 }
 
+func TestClosures(t *testing.T) {
+	input := ` let newAdder = fn(x) { fn(y) { x + y }; }; let addTwo = newAdder(2); addTwo(2);`
+
+	got := testEval(input)
+	testIntegerObject(t, got, 4)
+}
+
 func testEval(input string) object.Object {
 	le := lexer.New(input)
 	p := parser.New(le)
